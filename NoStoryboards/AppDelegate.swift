8a2501d0b49b37359b8fe224735befb2654bb979
Amir Sadoughi <amir.sadoughi@gmail.com>
2014-12-20 20:55:23 -0600

    Initial commit

--- NoStoryboards/AppDelegate.swift
+++ NoStoryboards/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        var mainViewController = ViewController()
+        window?.rootViewController = UINavigationController(rootViewController: mainViewController)
+        
+        window?.makeKeyAndVisible()
         return true
     }
 
